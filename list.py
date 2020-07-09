b3198501c29872cb8e1e8a78c2f26c0c9f463119
lyuyfshi <68063417+lyuyfshi@users.noreply.github.com>
2020-07-09 20:43:33 +0800

    python列表
    第一个Python列表文件

---
+++ list.py
@@ -0,0 +1,36 @@
+brands=['Apple']
+#name=['sunli','zhang qing','zhaozhi']
+
+#brands.extend(name)
+
+
+brands.insert(1,'huawei')
+
+# while  True:
+# 	name=input('请输入姓名')
+# 	if name=='q':
+# 		break
+
+# 	else:
+# 		brands.append(name)	
+
+
+
+
+
+# for i in range(len(brands)):
+
+# 	if brands[i]=='华为':
+# 		brands[i]='huawei'
+# 		break;
+
+print(brands)
+
+
+# for i in range(len(brands)-1):
+
+# 	if brands[i]=='panda' or brands[i]=='Apple':
+
+# 		del brands[i]
+
+# print(brands)
